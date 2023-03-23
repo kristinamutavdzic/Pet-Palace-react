ec75062b2159e2dc209b97ce308e1b741f3a55e7
Kristina Mutavdzic <mj20190244@student.fon.bg.ac.rs>
2023-03-23 02:17:21 +0100

    Uradjen izgled shop stranice i korpe

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Navbar} from "./components/navbar";
 import {Shop} from './pages/shop/Prodavnica'
 import {Cart} from './pages/cart/Korpa'
+import { ProizvodiContextProvider } from './context/ProizvodiContext';
 
 function App() {
   return (
-    <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Shop />}/>
-            <Route path="/cart" element={<Cart/>}/>
-          </Routes>
-        </Router>
+    <ProizvodiContextProvider>
+              <Router>
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<Shop />}/>
+                <Route path="/cart" element={<Cart/>}/>
+              </Routes>
+            </Router>
+      </ProizvodiContextProvider>
   );
 }
 
